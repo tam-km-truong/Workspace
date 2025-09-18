3270479903a37bc0c90ab700d5ab147a4cff0f8c
tam-km-truong <minhtamtruongkhac@gmail.com>
2025-09-18 18:52:27 +0200

    add dustbin unknown separation exp 070

--- experiments/070_PLOT_recompression_ATB/scripts/data_cleaning.R
+++ experiments/070_PLOT_recompression_ATB/scripts/data_cleaning.R
@@ -13,12 +13,51 @@ df_agc$size_MB <- df_agc$size_MB/1e+6
 df_agc <- df_agc %>% filter(name != "total")
 
 sum_mbgc <- sum(df_mbgc$size_MB)
-sum_xz <- sum(df_xz_orig$size_MB)
+sum_xz_orig <- sum(df_xz_orig$size_MB)
 sum_agc <- sum(df_agc$size_MB)
 
 df_plot <- data.frame(
-  group = c( "xz", "agc","mbgc"),
-  value = c( sum_xz, sum_agc, sum_mbgc)
+  group = c( "xz_orig", "agc","mbgc"),
+  value = c( sum_xz_orig, sum_agc, sum_mbgc)
 )
 
 write.csv(df_plot, "data/merged_plot_data.csv", row.names = FALSE)
+
+#Separation of dustbin and unknown batches
+sum_dustbin_agc <- sum(df_agc$size_MB[grepl("^dustbin", df_agc$name)])
+sum_dustbin_mbgc <- sum(df_mbgc$size_MB[grepl("^dustbin", df_mbgc$name)])
+sum_dustbin_xz_orig <- sum(df_xz_orig$size_MB[grepl("^dustbin", df_xz_orig$name)])
+
+sum_unknown_agc <- sum(df_agc$size_MB[grepl("^unknown", df_agc$name)])
+sum_unknown_mbgc <- sum(df_mbgc$size_MB[grepl("^unknown", df_mbgc$name)])
+sum_unknown_xz_orig <- sum(df_xz_orig$size_MB[grepl("^unknown", df_xz_orig$name)])
+
+sum_rest_agc <- sum(
+  df_agc$size_MB[
+    !grepl("^dustbin", df_agc$name) & !grepl("^unknown", df_agc$name)
+  ],
+  na.rm = TRUE
+)
+sum_rest_mbgc <- sum(
+  df_mbgc$size_MB[
+    !grepl("^dustbin", df_mbgc$name) & !grepl("^unknown", df_mbgc$name)
+  ],
+  na.rm = TRUE
+)
+sum_rest_xz_orig <- sum(
+  df_xz_orig$size_MB[
+    !grepl("^dustbin", df_xz_orig$name) & !grepl("^unknown", df_xz_orig$name)
+  ],
+  na.rm = TRUE
+)
+
+separation_df <- data.frame(
+  scheme = c("agc", "mbgc", "xz_orig"),
+  dustbin = c(sum_dustbin_agc, sum_dustbin_mbgc, sum_dustbin_xz_orig),
+  unknown = c(sum_unknown_agc, sum_unknown_mbgc, sum_unknown_xz_orig),
+  rest    = c(sum_rest_agc, sum_rest_mbgc, sum_rest_xz_orig)
+)
+
+write.csv(separation_df, "data/seperation_plot_data.csv", row.names = FALSE)
+
+
--- experiments/070_PLOT_recompression_ATB/scripts/plot.R
+++ experiments/070_PLOT_recompression_ATB/scripts/plot.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library("ggsci")
+library(tidyr)
 
 df_plot <- read_csv('data/merged_plot_data.csv')
 
@@ -7,28 +9,29 @@ df_plot$label <- paste0(round(df_plot$value / 1000, 1), "GB")
 df_plot$value_GB <- df_plot$value / 1000
 
 caption <- "\n
-    Parameters: AGC with -a -b 500 -s 1500; MBGC with -m 3. All with 25 threads.
-    In this experiment, All batches used in Sketree have 4000 genomes.
+    PARAMETERS: AGC with -a -b 500 -s 1500; MBGC with -m 3. All with 25 threads.
+    In this experiment, all batches of MiniPhy2 have 4000 genomes.
     \n
     Compression error on 1 batch (unknown_002).
     With MBGC batch unknown_002 couldn't compress, and with AGC, batch unknown_002 is corrupted
-    after compression. Max unknown batch size for MBGC is approx 720MB, AGC unknown_002 is approx 2GB"
+    after compression. Max unknown batch size for MBGC is approx 720MB, AGC unknown_002 is approx 2GB."
 
 # principal bar plot
 p <- ggplot(df_plot, aes(x = group, y = value_GB, fill = group)) +
   geom_col(color = "black", linewidth = 0.5) +
-  scale_fill_manual(values = c("steelblue", "tomato", "darkgreen")) +
+  #scale_fill_manual(values = c("steelblue", "tomato", "darkgreen")) +
+  scale_fill_npg() +
   labs(
     title = "Compression results - ATB",                  
     x = "Compression Scheme",                              
-    y = "Total Size (MB)" ,
-    caption = str_wrap(caption, width = 75)
+    y = "Total Size (GB)" ,
+    caption = str_wrap(caption, width = 100)
   ) +
-  scale_x_discrete(labels = c("XZ-Original", "AGC-SkeTree", "MBGC-Sketree")) +
+  scale_x_discrete(labels = c("MiniPhy-xz (original)", "MiniPhy2-AGC", "MiniPhy2-MBGC")) +
   theme_minimal(base_size = 14) + 
   theme(
     legend.position = "none",
-    plot.caption = element_text(hjust = 0.5, size = 12),
+    plot.caption = element_text(hjust = 0, vjust=0.2, size = 10),
     plot.title   = element_text(hjust = 0.5, size = 16)
   ) +
   #annotation
@@ -39,7 +42,54 @@ p
 w <- 20
 h <- 20
 u <- "cm"
-fn <- "results/compression_results_atb.pdf"
 
+fn <- "results/compression_results_atb.pdf"
 ggsave(fn, plot = p, width=w, height=h, units=u)
 
+
+df_sep_plot <- read_csv('data/seperation_plot_data.csv')
+
+flatten_df_sep <- df_sep_plot %>%
+  pivot_longer(
+    cols = c(dustbin, unknown, rest),
+    names_to = "group",
+    values_to = "size"
+  )
+
+flatten_df_sep$scheme <- factor(flatten_df_sep$scheme, levels = c("xz_orig", "agc", "mbgc"))
+flatten_df_sep$value_GB <- flatten_df_sep$size / 1000
+
+p2 <- ggplot(flatten_df_sep, aes(x = scheme, y = value_GB, fill = group)) +
+  geom_col(color = "black", linewidth = 0.5) +
+  scale_fill_npg(
+    labels = c("dustbin" = "Dustbin", 
+               "unknown" = "Unknown", 
+               "rest" = "Species"),
+    name = "Batch types"
+  ) +
+  labs(
+    title = "Compression results - ATB",                  
+    x = "Compression Scheme",                              
+    y = "Size (GB)" ,
+    caption = str_wrap(caption, width = 90)
+  ) +
+  scale_x_discrete(labels = c("MiniPhy-xz (original)", "MiniPhy2-AGC", "MiniPhy2-MBGC")) +
+  theme_minimal(base_size = 14) +
+  theme_minimal(base_size = 14) + 
+  theme(
+    plot.caption = element_text(hjust = 0, vjust=0.2, size = 10),
+    plot.title   = element_text(hjust = 0.5, size = 16)
+  )+
+  #annotation
+  geom_text(aes(label = paste0(round(value_GB, 1), "GB")), 
+            position = position_stack(vjust = 0.5), 
+            size = 3, color = "white") +
+  geom_text(data = df_plot, 
+            aes(x = group, y = value, label = label), 
+            inherit.aes = FALSE, 
+            vjust = -0.5, size = 4, fontface = "bold")
+
+p2
+
+fn2 <- "results/compression_results_atb_separated.pdf"
+ggsave(fn2, plot = p2, width=w, height=h, units=u)
